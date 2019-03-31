6512386549335b21c24a293e47534a4feb7d0b23
Rider Mamani <rdrmamani@gmail.com>
2019-03-31 06:51:10 -0400

    dist añadido

---
+++ dist/precache-manifest.af9c30368d437548d090c224698da349.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa/dist/robots.txt"
+  },
+  {
+    "revision": "d46894c7453b30361020",
+    "url": "/vue-pwa/dist/js/chunk-vendors.588b6f36.js"
+  },
+  {
+    "revision": "5baf0f156191e9b97b7a",
+    "url": "/vue-pwa/dist/js/app.1aa4d129.js"
+  },
+  {
+    "revision": "d7c849ad540c6c2a0455",
+    "url": "/vue-pwa/dist/js/about.ad763791.js"
+  },
+  {
+    "revision": "9dadac484c0367ec509670ed955e339b",
+    "url": "/vue-pwa/dist/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa/dist/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "5baf0f156191e9b97b7a",
+    "url": "/vue-pwa/dist/css/app.80f484af.css"
+  }
+];
